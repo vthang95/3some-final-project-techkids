5e957d9306f405e71ccf40cdcabb8e8780f1f4a2
vthang95 <tranvietthang95@gmail.com>
2017-05-25 18:18:40 +0700

    added APIs: search users by name and get single user by name

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -4,5 +4,7 @@ const Router = express.Router();
 const userController = require('./users.controller');
 
 Router.post('/signup', userController.postSignup);
+Router.get('/search', userController.getSearchUserByEmail);
+Router.get('/:username', userController.getUserByUsername);
 
 module.exports = Router;
