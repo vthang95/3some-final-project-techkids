75c0f34fddcecac7d55274e9ed453a70c686048e
vthang95 <tranvietthang95@gmail.com>
2017-06-25 08:24:55 +0700

    change http to https

--- src/app/utils/helper.js
+++ src/app/utils/helper.js
@@ -1,6 +1,6 @@
 const port = process.env.PORT || 7000;
 
-export const getHostName = () => `http://localhost:${port}`;
+export const getHostName = () => `https://localhost:${port}`;
 export const getUserId = () => {
   let user_id = localStorage.getItem('user_id');
   return user_id;
